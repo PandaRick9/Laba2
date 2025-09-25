1f41c452b3ad3cfb56003e1eef3ebe90f8c8c233
PandaRick9 <herettyr765@gmail.com>
2025-09-26 00:50:18 +0300

    Optimize add method for zero values

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,13 +1,14 @@
 public class Calculator {
 
-    public int add(int a, int b) {
-        return a + b;
-    }
-
     public int subtract(int a, int b) {
         return a - b;
     }
 
+    public int add(int a, int b) {
+        if (a == 0) return b;
+        if (b == 0) return a;
+        return a + b;
+    }
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println("5 + 3 = " + calc.add(5, 3));
