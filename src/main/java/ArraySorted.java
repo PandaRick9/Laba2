d7bb298d5937ff66d0a9dfb786124471383569a5
PandaRick9 <herettyr765@gmail.com>
2025-09-26 00:03:53 +0300

    connect bubblesort

--- src/main/java/ArraySorted.java
+++ src/main/java/ArraySorted.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 
 public class ArraySorted {
     public static void main(String[] args) {
@@ -7,9 +8,21 @@ public class ArraySorted {
         System.out.println("Original array:");
         printArray(array);
         System.out.println();
+        bubbleSort(array);
 
     }
-
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
     public static void printArray(int[] arr) {
         for (int j : arr) { System.out.print(j + " "); }
         System.out.println();
