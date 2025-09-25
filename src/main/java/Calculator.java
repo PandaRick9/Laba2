d8d7aed1417f340e4c623cde1ed55527dd0127d1
PandaRick9 <herettyr765@gmail.com>
2025-09-26 00:44:27 +0300

    Add basic Calculator class

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("5 + 3 = " + calc.add(5, 3));
+        System.out.println("5 - 3 = " + calc.subtract(5, 3));
+    }
+}
