e1491fc52629c0c3c5bb0c9aeed6735a9f92459c
PandaRick9 <herettyr765@gmail.com>
2025-09-26 01:05:47 +0300

    Integrate Calculator into HelloWorld

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,12 +2,10 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;public class HelloWorld {
     public static void main(String[] args) {
-        String text = "Hi!, Hello Vitaly!";
-        try (OutputStreamWriter writer = new OutputStreamWriter(System.out, StandardCharsets.UTF_8)) {
-            writer.write(text);
-            writer.flush(); // Убедимся, что всё записано
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println("Calculator Application");
+
+        Calculator calc = new Calculator();
+        System.out.println("10 + 5 = " + calc.add(10, 5));
+        System.out.println("10 - 5 = " + calc.subtract(10, 5));
     }
 }
